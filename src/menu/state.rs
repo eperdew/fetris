2d4686b192caf30e8a6a81f5140b41c7e6f6a5ec
Eric Perdew <eperdew17@gmail.com>
2026-04-27 23:52:31 -0400

    feat(debug): add MenuScreen::Debug and MenuState::debug_unlocked

--- src/menu/state.rs
+++ src/menu/state.rs
@@ -9,6 +9,7 @@ pub struct MenuState {
     pub game_mode: GameMode,
     pub rotation: Kind,
     pub hi_scores_tab: usize,
+    pub debug_unlocked: bool,
 }
 
 #[derive(Clone, Copy, PartialEq, Eq)]
@@ -16,6 +17,7 @@ pub enum MenuScreen {
     Main,
     HiScores,
     Controls,
+    Debug,
 }
 
 impl MenuState {
@@ -27,6 +29,7 @@ impl MenuState {
             game_mode: config.game_mode,
             rotation: config.rotation,
             hi_scores_tab: 0,
+            debug_unlocked: false,
         }
     }
 }
@@ -39,6 +42,7 @@ impl Default for MenuState {
             game_mode: GameMode::Master,
             rotation: Kind::Ars,
             hi_scores_tab: 0,
+            debug_unlocked: false,
         }
     }
 }
